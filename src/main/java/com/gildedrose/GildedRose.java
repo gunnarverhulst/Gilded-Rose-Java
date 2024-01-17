3021527b86a1bdddf3036daf857c2131284003dd
Gunz <verhulstg@gmail.com>
2024-01-17 18:51:16 +0100

    Reposition calculation of qualityChangeForToday to ItemType as ItemType determines the possible result.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -6,7 +6,6 @@ import java.util.List;
 class GildedRose {
     private static final int MAX_QUALITY_VALUE = 50;
     private static final int MIN_QUALITY_VALUE = 0;
-    private static final int MIN_SELLIN_VALUE = 0;
     private final List<Item> itemList;
 
     public GildedRose(Item[] items) {
@@ -37,7 +36,7 @@ class GildedRose {
             item.sellIn--;
         }
 
-        int qualityChangeForToday = calculateQualityChangeForToday(item, itemType);
+        int qualityChangeForToday = ItemType.calculateQualityChangeForToday(item, itemType);
 
         if(itemType != ItemType.SULFURAS)
             item.quality = applyQualityChangeForToday(item, qualityChangeForToday);
@@ -51,34 +50,6 @@ class GildedRose {
         if(item.quality < MIN_QUALITY_VALUE){
             item.quality = MIN_QUALITY_VALUE;
         }
-    }
-
-    private int calculateQualityChangeForToday(Item item, ItemType itemType) {
-        int qualityChangeForToday = 0;
-        if(itemType == ItemType.AGED_BRIE )
-            qualityChangeForToday = isSellInLessThanZero(item.sellIn) ? 2 : 1;
-        else if (itemType == ItemType.BACKSTAGE_PASS)
-            qualityChangeForToday = calculateQualityChangeTodayForBackstagePass(item);
-        else if (itemType == ItemType.CONJURED_ITEM)
-            qualityChangeForToday = isSellInLessThanZero(item.sellIn) ? -4 : -2;
-        else if (itemType != ItemType.SULFURAS)
-            qualityChangeForToday = isSellInLessThanZero(item.sellIn) ? -2 : -1;
-        return qualityChangeForToday;
-    }
-
-    private static boolean isSellInLessThanZero(int itemSellInvValue) {
-        return itemSellInvValue < MIN_SELLIN_VALUE;
-    }
-
-    private int calculateQualityChangeTodayForBackstagePass(Item item) {
-        if (isSellInLessThanZero(item.sellIn))
-            return -item.quality;
-        else if (item.sellIn < 6)
-            return 3;
-        else if (item.sellIn < 11)
-            return 2;
-
-        return 1;
     }
 
     private static int applyQualityChangeForToday(Item item, int qualityChangeForToday) {
--- src/main/java/com/gildedrose/ItemType.java
+++ src/main/java/com/gildedrose/ItemType.java
@@ -1,11 +1,14 @@
 package com.gildedrose;
 
+
 public enum ItemType {
     AGED_BRIE,
     BACKSTAGE_PASS,
     SULFURAS,
     CONJURED_ITEM,
     REGULAR_ITEM;
+
+    private static final int MIN_SELLIN_VALUE = 0;
 
     public static ItemType convertToItemType(String itemString){
         String preppedString = itemString.startsWith("Conjured") ? "Conjured" : itemString;
@@ -17,6 +20,34 @@ public enum ItemType {
             case "Conjured" -> CONJURED_ITEM;
             default -> REGULAR_ITEM;
         };
+    }
+
+    public static int calculateQualityChangeForToday(Item item, ItemType itemType) {
+        int qualityChangeForToday = 0;
+        if(itemType == ItemType.AGED_BRIE )
+            qualityChangeForToday = isSellInLessThanZero(item.sellIn) ? 2 : 1;
+        else if (itemType == ItemType.BACKSTAGE_PASS)
+            qualityChangeForToday = calculateQualityChangeTodayForBackstagePass(item);
+        else if (itemType == ItemType.CONJURED_ITEM)
+            qualityChangeForToday = isSellInLessThanZero(item.sellIn) ? -4 : -2;
+        else if (itemType != ItemType.SULFURAS)
+            qualityChangeForToday = isSellInLessThanZero(item.sellIn) ? -2 : -1;
+        return qualityChangeForToday;
+    }
+
+    private static boolean isSellInLessThanZero(int itemSellInValue) {
+        return itemSellInValue < MIN_SELLIN_VALUE;
+    }
+
+    private static int calculateQualityChangeTodayForBackstagePass(Item item) {
+        if (isSellInLessThanZero(item.sellIn))
+            return -item.quality;
+        else if (item.sellIn < 6)
+            return 3;
+        else if (item.sellIn < 11)
+            return 2;
+
+        return 1;
     }
 
 }
