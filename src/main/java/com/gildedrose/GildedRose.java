15fc93f75d41db1b48399337788a38f8d40bf15b
Gunz <verhulstg@gmail.com>
2024-01-17 12:01:07 +0100

    Refactor extracting min and max quality checks into methods. Introducing min max constants for quality

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,8 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final int MAX_QUALITY_VALUE = 50;
+    public static final int MIN_QUALITY_VALUE = 0;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -14,44 +16,52 @@ class GildedRose {
             }
 
             if(item.name.equals("Aged Brie") ) {
-                if (item.quality < 50) {
+                if (isQualityLessThanMax(item.quality)) {
                     item.quality++;
                 }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
+                if (isQualityLessThanMax(item.quality)) {
                     item.quality++;
 
                     if (item.sellIn < 11) {
-                        if (item.quality < 50) {
+                        if (isQualityLessThanMax(item.quality)) {
                             item.quality++;
                         }
                     }
 
                     if (item.sellIn < 6) {
-                        if (item.quality < 50) {
+                        if (isQualityLessThanMax(item.quality)) {
                             item.quality++;
                         }
                     }
                 }
             } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (item.quality > 0) {
+                if (isQualityMoreThanMin(item)) {
                     item.quality--;
                 }
             }
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
+                    if (isQualityLessThanMax(item.quality)) {
                         item.quality++;
                     }
                 } else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
                     item.quality = item.quality - item.quality;
                 } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    if (item.quality > 0) {
+                    if (isQualityMoreThanMin(item)) {
                         item.quality--;
                     }
                 }
             }
         }
+    }
+
+    private static boolean isQualityMoreThanMin(Item item) {
+        return item.quality > MIN_QUALITY_VALUE;
+    }
+
+    private static boolean isQualityLessThanMax(int itemQualityValue) {
+        return itemQualityValue < MAX_QUALITY_VALUE;
     }
 }
