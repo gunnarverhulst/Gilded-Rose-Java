040c3df7f05ac5a82f66af94896e28a15519f5e3
Gunz <verhulstg@gmail.com>
2024-01-17 09:51:40 +0100

    Understanding workings via test One run HOS, Regular item

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,29 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void elixirMongooseOneRun() {
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 7) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Elixir of the Mongoose", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(6, app.items[0].quality);
+    }
+
+    @Test
+    void handOfSulfurasOneRun(){
+
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[1].name);
+        assertEquals(-1, app.items[1].sellIn);
+        assertEquals(80, app.items[1].quality);
+    }
+
 }
