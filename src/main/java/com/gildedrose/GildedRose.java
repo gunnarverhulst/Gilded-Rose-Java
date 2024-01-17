c6866f5c6c882dbd041b77957c70cc702bec2dde
Gunz <verhulstg@gmail.com>
2024-01-17 11:39:20 +0100

    Refactor reducing one level of if first block

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,29 +9,29 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if(item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            if(item.name.equals("Aged Brie") ) {
+                if (item.quality < 50) {
+                    item.quality++;
+                }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality < 50) {
                     item.quality++;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality++;
                         }
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality++;
                         }
                     }
                 }
-            } else {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    if (item.quality > 0) {
-                        item.quality--;
-                    }
+            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) {
+                    item.quality--;
                 }
             }
 
